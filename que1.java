335b7ecafd603c5cee787eccb96b5e3cae37f127
Dulana Dewmin <74188883+DulanaDewmin@users.noreply.github.com>
2024-05-05 23:30:54 +0530

    que 1

---
+++ que1.java
@@ -0,0 +1,10 @@
+public class que1 {
+
+    public static void main(String[] args) {
+        System.out.println("ASCII Chart: ");
+        for (int i = 0; i < 128; i++) {
+            // System.out.println(i + " : " + (char) i);
+            System.out.printf("%3d: %c\n", i, i);
+        }
+    }
+}
