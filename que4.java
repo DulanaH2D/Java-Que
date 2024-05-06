2e8fe16feb46bf4307de0414d365b61d166a1701
Dulana Dewmin <74188883+DulanaDewmin@users.noreply.github.com>
2024-05-06 09:56:07 +0530

    que 4,5

---
+++ que4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class que4 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int num = myObj.nextInt();
+
+        if (num / 2 == 0) {
+            System.out.println("number is even");
+        } else {
+            System.out.println("number is odd");
+        }
+
+    }
+}
---
+++ que5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class que5 {
+    static boolean isTriangular(int num) {
+        if (num < 0)
+            return false;
+
+        int sum = 0;
+
+        for (int n = 1; sum <= num; n++) {
+            sum = sum + n;
+            if (sum == num)
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter number");
+        Scanner myObj = new Scanner(System.in);
+        int n = myObj.nextInt();
+        if (isTriangular(n))
+            System.out.print("The number "
+                    + "is a triangular number");
+        else
+            System.out.print("The number"
+                    + " is NOT a triangular number");
+    }
+
+}
