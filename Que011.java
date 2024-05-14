bb26ea2d10e6a13767025a92e109a739a2045eab
Dulana Dewmin <74188883+DulanaDewmin@users.noreply.github.com>
2024-05-14 17:30:12 +0530

    Que011, Que012 create and Que009/010 modify

--- Que011.java
+++ Que011.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
-public class Que010 {
+public class Que011 {
     public static void main(String[] args) {
         try {
-            System.out.println("Enter a octal number: ");
+            System.out.println("Enter a binary number: ");
             Scanner sc = new Scanner(System.in);
             String n = sc.nextLine();
 
-            int decimal = Integer.parseInt(n, 8);
+            int decimal = Integer.parseInt(n, 2);
             System.out.println("Decimal: " + decimal);
 
             sc.close();
         } catch (Exception e) {
-            System.out.println("Invalid octal number.");
+            System.out.println("Invalid binary number.");
         }
     }
-
 }
--- Que012.java
+++ Que012.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
-public class Que010 {
+public class Que012 {
     public static void main(String[] args) {
         try {
-            System.out.println("Enter a octal number: ");
+            System.out.println("Enter a hexadecimal number: ");
             Scanner sc = new Scanner(System.in);
             String n = sc.nextLine();
 
-            int decimal = Integer.parseInt(n, 8);
+            int decimal = Integer.parseInt(n, 16);
             System.out.println("Decimal: " + decimal);
 
             sc.close();
         } catch (Exception e) {
-            System.out.println("Invalid octal number.");
+            System.out.println("Invalid hexadecimal number.");
         }
     }
-
 }
